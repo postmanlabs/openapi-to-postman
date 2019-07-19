7428188e284eef640e04d68cd4f1adb7a6412a60
Dhroov Gupta <dhroov.gupta@getpostman.com>
2019-07-19 14:14:38 +0530

    bug fixed for using local server instead of global server

--- test/unit/base.test.js
+++ test/unit/base.test.js
@@ -11,8 +11,9 @@ describe('CONVERT FUNCTION TESTS ', function() {
     var pathPrefix = VALID_OPENAPI_PATH + '/test.json',
       specPath = path.join(__dirname, pathPrefix),
       pathPrefix1 = VALID_OPENAPI_PATH + '/test1.json',
-      specPath1 = path.join(__dirname, pathPrefix1);
-
+      specPath1 = path.join(__dirname, pathPrefix1),
+      pathPrefix2 = VALID_OPENAPI_PATH + '/server_overriding.json',
+      specPath2 = path.join(__dirname, pathPrefix2);
 
     it('Should generate collection conforming to schema for and fail if not valid ' +
      specPath, function(done) {
@@ -36,6 +37,22 @@ describe('CONVERT FUNCTION TESTS ', function() {
         expect(conversionResult.output[0].type).to.equal('collection');
         expect(conversionResult.output[0].data).to.have.property('info');
         expect(conversionResult.output[0].data).to.have.property('item');
+
+        done();
+      });
+    });
+    it('Should create a request using local server instead of global server ' +
+    specPath2, function(done) {
+      Converter.convert({ type: 'file', data: specPath2 }, { schemaFaker: true }, (err, conversionResult) => {
+        expect(err).to.be.null;
+        expect(conversionResult.result).to.equal(true);
+        expect(conversionResult.output.length).to.equal(1);
+        expect(conversionResult.output[0].type).to.equal('collection');
+        expect(conversionResult.output[0].data).to.have.property('info');
+        expect(conversionResult.output[0].data).to.have.property('item');
+        expect(conversionResult.output[0].data.item[1].request.url.path).to.be.an('array');
+        expect(conversionResult.output[0].data.item[1].request.url.path).to.have.lengthOf(2);
+        expect(conversionResult.output[0].data.item[1].request.url.host).to.be.an('array');
 
         done();
       });
