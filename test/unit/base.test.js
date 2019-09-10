cc2ba8b8fb423128e8d26dd6bd603944e128d8c9
Dhroov7 <dhroov.gupta@getpostman.com>
2019-09-10 17:01:47 +0530

    Added more tests for creating folder strategy using tags

--- test/unit/base.test.js
+++ test/unit/base.test.js
@@ -147,109 +147,129 @@ describe('CONVERT FUNCTION TESTS ', function() {
             done();
           });
       });
-    });
-  });
-  describe('for invalid requestNameSource option', function() {
-    var pathPrefix = VALID_OPENAPI_PATH + '/test1.json',
-      specPath = path.join(__dirname, pathPrefix);
 
-    it('for invalid request name, converter should throw an error', function(done) {
-      var openapi = fs.readFileSync(specPath, 'utf8');
-      Converter.convert({ type: 'string', data: openapi }, { requestNameSource: 'uKnown' }, (err, conversionResult) => {
-        expect(err).to.be.null;
-        expect(conversionResult.reason).to.equal(
-          'requestNameSource (uKnown) in options is invalid or property does not exist in pets');
-        done();
+      it('should create the folder when tag is only given in the operation and not in the root level', function(done) {
+        Converter.convert({ type: 'file', data: tagsFolderSpec },
+          { folderStrategy: 'tag', schemaFaker: true }, (err, conversionResult) => {
+            expect(conversionResult.output[0].data.item[2].name).to.equal('pets');
+            expect(conversionResult.output[0].data.item[2].item[0].request.name).to.equal('List all pets');
+            expect(conversionResult.output[0].data.item[2].item[1].request.name).to.equal('Create pets');
+            done();
+          });
+      });
+
+      it('should create the folder when tag is defined at root level and also used in the operation', function(done) {
+        Converter.convert({ type: 'file', data: tagsFolderSpec },
+          { folderStrategy: 'tag', schemaFaker: true }, (err, conversionResult) => {
+            expect(conversionResult.output[0].data.item[1].name).to.equal('single pet');
+            expect(conversionResult.output[0].data.item[1].item[0].request.name).to.equal('Info for a specific pet');
+            done();
+          });
       });
     });
-  });
-});
+    describe('for invalid requestNameSource option', function() {
+      var pathPrefix = VALID_OPENAPI_PATH + '/test1.json',
+        specPath = path.join(__dirname, pathPrefix);
 
-/* Plugin Interface Tests */
-describe('INTERFACE FUNCTION TESTS ', function () {
-  describe('The converter must identify valid specifications', function () {
-    var pathPrefix = VALID_OPENAPI_PATH,
-      sampleSpecs = fs.readdirSync(path.join(__dirname, pathPrefix));
-
-    sampleSpecs.map((sample) => {
-      var specPath = path.join(__dirname, pathPrefix, sample);
-
-      it(specPath + ' is valid ', function(done) {
-        var openapi = fs.readFileSync(specPath, 'utf8'),
-          validationResult = Converter.validate({ type: 'string', data: openapi });
-
-        expect(validationResult.result).to.equal(true);
-        done();
-      });
-    });
-  });
-  describe('The converter must identify invalid specifications', function () {
-    var pathPrefix = INVALID_OPENAPI_PATH,
-      sampleSpecs = fs.readdirSync(path.join(__dirname, pathPrefix));
-
-    sampleSpecs.map((sample) => {
-      var specPath = path.join(__dirname, pathPrefix, sample);
-
-      it(specPath + ' is invalid ', function(done) {
-        var openapi = fs.readFileSync(specPath, 'utf8'),
-          validationResult = Converter.validate({ type: 'string', data: openapi });
-
-        expect(validationResult.result).to.equal(false);
-        Converter.convert({ type: 'string', data: openapi }, {}, function(err, conversionResult) {
-          expect(err).to.be.null;
-          expect(conversionResult.result).to.equal(false);
-          done();
-        });
-      });
-    });
-  });
-
-  describe('The converter must generate a collection conforming to the schema', function () {
-    var pathPrefix = VALID_OPENAPI_PATH,
-      sampleSpecs = fs.readdirSync(path.join(__dirname, pathPrefix));
-
-    sampleSpecs.map((sample) => {
-      var specPath = path.join(__dirname, pathPrefix, sample);
-      it('Should generate collection conforming to schema for and fail if not valid ' + specPath, function(done) {
-        // var openapi = fs.readFileSync(specPath, 'utf8');
-        var result = Converter.validate({ type: 'file', data: specPath });
-        expect(result.result).to.equal(true);
-        Converter.convert({ type: 'file', data: specPath },
-          {}, (err, conversionResult) => {
+      it('for invalid request name, converter should throw an error', function(done) {
+        var openapi = fs.readFileSync(specPath, 'utf8');
+        Converter.convert({ type: 'string', data: openapi }, { requestNameSource: 'uKnown' },
+          (err, conversionResult) => {
             expect(err).to.be.null;
-
-            expect(conversionResult.result).to.equal(true);
-            expect(conversionResult.output.length).to.equal(1);
-            expect(conversionResult.output[0].type).to.equal('collection');
-            expect(conversionResult.output[0].data).to.have.property('info');
-            expect(conversionResult.output[0].data).to.have.property('item');
-
+            expect(conversionResult.reason).to.equal(
+              'requestNameSource (uKnown) in options is invalid or property does not exist in pets');
             done();
           });
       });
     });
   });
 
-  describe('The converter must throw an error for invalid input type', function() {
-    it('(type: some invalid value)', function(done) {
-      var result = Converter.validate({ type: 'fil', data: 'invalid_path' });
-      expect(result.result).to.equal(false);
-      expect(result.reason).to.equal('input type is not valid');
-      Converter.convert({ type: 'fil', data: 'invalid_path' }, {}, function(err, conversionResult) {
-        expect(conversionResult.result).to.equal(false);
-        expect(conversionResult.reason).to.equal('input type:fil is not valid');
-        done();
+  /* Plugin Interface Tests */
+  describe('INTERFACE FUNCTION TESTS ', function () {
+    describe('The converter must identify valid specifications', function () {
+      var pathPrefix = VALID_OPENAPI_PATH,
+        sampleSpecs = fs.readdirSync(path.join(__dirname, pathPrefix));
+
+      sampleSpecs.map((sample) => {
+        var specPath = path.join(__dirname, pathPrefix, sample);
+
+        it(specPath + ' is valid ', function(done) {
+          var openapi = fs.readFileSync(specPath, 'utf8'),
+            validationResult = Converter.validate({ type: 'string', data: openapi });
+
+          expect(validationResult.result).to.equal(true);
+          done();
+        });
       });
     });
-  });
+    describe('The converter must identify invalid specifications', function () {
+      var pathPrefix = INVALID_OPENAPI_PATH,
+        sampleSpecs = fs.readdirSync(path.join(__dirname, pathPrefix));
 
-  describe('The converter must throw an error for invalid input path', function() {
-    it('(type: file)', function(done) {
-      var result = Converter.validate({ type: 'file', data: 'invalid_path' });
-      expect(result.result).to.equal(false);
-      Converter.convert({ type: 'file', data: 'invalid_path' }, {}, function(err) {
-        expect(err.toString()).to.equal('Error: ENOENT: no such file or directory, open \'invalid_path\'');
-        done();
+      sampleSpecs.map((sample) => {
+        var specPath = path.join(__dirname, pathPrefix, sample);
+
+        it(specPath + ' is invalid ', function(done) {
+          var openapi = fs.readFileSync(specPath, 'utf8'),
+            validationResult = Converter.validate({ type: 'string', data: openapi });
+
+          expect(validationResult.result).to.equal(false);
+          Converter.convert({ type: 'string', data: openapi }, {}, function(err, conversionResult) {
+            expect(err).to.be.null;
+            expect(conversionResult.result).to.equal(false);
+            done();
+          });
+        });
+      });
+    });
+
+    describe('The converter must generate a collection conforming to the schema', function () {
+      var pathPrefix = VALID_OPENAPI_PATH,
+        sampleSpecs = fs.readdirSync(path.join(__dirname, pathPrefix));
+
+      sampleSpecs.map((sample) => {
+        var specPath = path.join(__dirname, pathPrefix, sample);
+        it('Should generate collection conforming to schema for and fail if not valid ' + specPath, function(done) {
+        // var openapi = fs.readFileSync(specPath, 'utf8');
+          var result = Converter.validate({ type: 'file', data: specPath });
+          expect(result.result).to.equal(true);
+          Converter.convert({ type: 'file', data: specPath },
+            {}, (err, conversionResult) => {
+              expect(err).to.be.null;
+
+              expect(conversionResult.result).to.equal(true);
+              expect(conversionResult.output.length).to.equal(1);
+              expect(conversionResult.output[0].type).to.equal('collection');
+              expect(conversionResult.output[0].data).to.have.property('info');
+              expect(conversionResult.output[0].data).to.have.property('item');
+
+              done();
+            });
+        });
+      });
+    });
+
+    describe('The converter must throw an error for invalid input type', function() {
+      it('(type: some invalid value)', function(done) {
+        var result = Converter.validate({ type: 'fil', data: 'invalid_path' });
+        expect(result.result).to.equal(false);
+        expect(result.reason).to.equal('input type is not valid');
+        Converter.convert({ type: 'fil', data: 'invalid_path' }, {}, function(err, conversionResult) {
+          expect(conversionResult.result).to.equal(false);
+          expect(conversionResult.reason).to.equal('input type:fil is not valid');
+          done();
+        });
+      });
+    });
+
+    describe('The converter must throw an error for invalid input path', function() {
+      it('(type: file)', function(done) {
+        var result = Converter.validate({ type: 'file', data: 'invalid_path' });
+        expect(result.result).to.equal(false);
+        Converter.convert({ type: 'file', data: 'invalid_path' }, {}, function(err) {
+          expect(err.toString()).to.equal('Error: ENOENT: no such file or directory, open \'invalid_path\'');
+          done();
+        });
       });
     });
   });
