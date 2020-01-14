f9adc10459ab3fa8aaa1c852f18503f06bbe1273
umeshp7 <31915995+umeshp7@users.noreply.github.com>
2020-01-14 16:21:59 +0530

    Support for multi file schema in schemapack

--- index.js
+++ index.js
@@ -1,14 +1,6 @@
 'use strict';
 
-const SchemaPack = require('./lib/schemapack.js').SchemaPack,
-  async = require('async'),
-  parse = require('./lib/parse.js'),
-
-  // options for oas-resolver
-  OasResolverOptions = {
-    resolve: true, // Resolve external references
-    jsonSchema: true // Treat $ref like JSON Schema and convert to OpenAPI Schema Objects
-  };
+const SchemaPack = require('./lib/schemapack.js').SchemaPack;
 
 module.exports = {
   // Old API wrapping the new API
@@ -18,86 +10,17 @@ module.exports = {
     if (schema.validated) {
       return schema.convert(cb);
     }
-    else if (input.type === 'folder') {
-      let filesPathArray = input.data,
-        convertedSpecs = [],
-        rootFiles = parse.getRootFiles(filesPathArray);
-
-      async.eachSeries(rootFiles, (rootFile, callback) => {
-        parse
-        // will merge all the files in the folder
-          .mergeFiles(rootFile, OasResolverOptions)
-          .then((spec) => {
-            converter.convert(spec, options, (err, result) => {
-              if (err) {
-                return callback(err);
-              }
-              convertedSpecs.push(result);
-              return callback(null);
-            });
-          })
-          .catch((err) => {
-            return callback(err);
-          });
-      }, (err) => {
-
-        if (err) {
-          return cb({
-            result: false,
-            reason: 'input type:' + input.type + ' is not valid'
-          });
-        }
-
-        var conversionResult = false,
-          convertedCollections = [],
-          reasonForFail;
-
-        _.forEach(convertedSpecs, (convertedSpec) => {
-          if (convertedSpec.result) {
-            let result;
-            conversionResult = conversionResult || convertedSpec.result;
-
-            function returnCollection (resultSpec) {
-              if (resultSpec.type === 'collection') {
-                return true;
-              }
-            }
-
-            // filtering out the collections from the convertedSpec
-            result = convertedSpec.output.filter(returnCollection);
-            convertedCollections.push(result[0]);
-          }
-          else {
-            conversionResult = conversionResult || convertedSpec.result;
-            reasonForFail = convertedSpec.reason;
-          }
-        });
-
-        if (conversionResult) {
-          return cb(null, {
-            result: true,
-            output: convertedCollections
-          });
-        }
-
-        return cb(null, {
-          result: false,
-          reason: reasonForFail
-        });
-      });
-    }
-    // eslint-disable-next-line no-else-return
-    else {
-      return cb(null, {
-        result: false,
-        reason: 'input type:' + input.type + ' is not valid'
-      });
-    }
+    return cb(null, schema.validationResult);
   },
 
-  validate: function(input) {
+  validate: function (input) {
     var schema = new SchemaPack(input);
     return schema.validationResult;
+  },
+
+  mergeAndValidate: function (input, cb) {
+    var schema = new SchemaPack(input);
+    schema.mergeAndValidate(cb);
   },
 
   getOptions: function() {
