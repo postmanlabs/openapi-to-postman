9b57c496ee16df5b592880849437ed3cc775ec54
Ed Clements <ed.clements@gmail.com>
2020-09-18 16:08:38 +0100

    Fix structure test

--- test/system/structure.test.js
+++ test/system/structure.test.js
@@ -17,7 +17,8 @@ const optionIds = [
     'validateMetadata',
     'ignoreUnresolvedVariables',
     'optimizeConversion',
-    'strictRequestMatching'
+    'strictRequestMatching',
+    'disableOptionalParameters'
   ],
   expectedOptions = {
     collapseFolders: {
