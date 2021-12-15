ee8e6da5a43a240936de6ecb9042b7d83cc4a7a7
Erik Mendoza <efernandomendoza@gmail.com>
2021-12-14 22:29:22 -0600

    Adding support to nested schemas with examples, adding support to nested types as array, adding support to outer required property in deref

--- test/unit/31Xsupport/schemapack.test.js
+++ test/unit/31Xsupport/schemapack.test.js
@@ -4,24 +4,40 @@ const expect = require('chai').expect,
   fs = require('fs'),
   path = require('path'),
   OPENAPI_31_FOLDER = '../../data/valid_openapi31X',
-  OPENAPI_30_FOLDER = '../../data/valid_openapi';
+  OPENAPI_30_FOLDER = '../../data/valid_openapi',
+  _ = require('lodash'),
+  petstoreTransactions = require('./../../data/31CollectionTransactions/petstoreGeneratedCollection.json');
+
+function getAllTransactions (collection, allRequests) {
+  if (!_.has(collection, 'item') || !_.isArray(collection.item)) {
+    return;
+  }
+  _.forEach(collection.item, (item) => {
+    if (_.has(item, 'request') || _.has(item, 'response')) {
+      allRequests.push(item);
+    }
+    else {
+      getAllTransactions(item, allRequests);
+    }
+  });
+}
 
 describe('Testing openapi 3.1 schema pack convert', function() {
-  // it('Should convert from openapi 3.1 spec to postman collection', function() {
-  //   const fileSource = path.join(__dirname, OPENAPI_30_FOLDER + '/petstore-detailed.yaml'),
-  //     fileData = fs.readFileSync(fileSource, 'utf8'),
-  //     input = {
-  //       type: 'string',
-  //       data: fileData
-  //     },
-  //     converter = new SchemaPack(input);
+  it('Should convert from openapi 3.1 spec to postman collection -- multiple refs', function() {
+    const fileSource = path.join(__dirname, OPENAPI_31_FOLDER + '/multiple_refs.json'),
+      fileData = fs.readFileSync(fileSource, 'utf8'),
+      input = {
+        type: 'string',
+        data: fileData
+      },
+      converter = new SchemaPack(input);
 
-  //   converter.convert((err, result) => {
-  //     expect(err).to.be.null;
-  //   });
-  // });
+    converter.convert((err, result) => {
+      expect(err).to.be.null;
+    });
+  });
 
-  it('Should convert from openapi 3.1 spec to postman collection -- petstore modifyed', function() {
+  it('Should convert from openapi 3.1 spec to postman collection -- petstore', function() {
     const fileSource = path.join(__dirname, OPENAPI_31_FOLDER + '/petstore.json'),
       fileData = fs.readFileSync(fileSource, 'utf8'),
       input = {
@@ -35,17 +51,45 @@ describe('Testing openapi 3.1 schema pack convert', function() {
     });
   });
 
-  // it('Should convert from openapi 3.0 spec to postman collection', function() {
-  //   const fileSource = path.join(__dirname, OPENAPI_30_FOLDER + '/petstore.json'),
-  //     fileData = fs.readFileSync(fileSource, 'utf8'),
-  //     input = {
-  //       type: 'json',
-  //       data: fileData
-  //     },
-  //     converter = new SchemaPack(input);
+  it('Should convert from openapi 3.1 spec to postman collection -- multiple refs outer required', function() {
+    const fileSource = path.join(__dirname, OPENAPI_31_FOLDER + '/multiple_refs_outer_required.json'),
+      fileData = fs.readFileSync(fileSource, 'utf8'),
+      input = {
+        type: 'string',
+        data: fileData
+      },
+      converter = new SchemaPack(input);
 
-  //   converter.convert((err, result) => {
-  //     expect(err).to.be.null;
-  //   });
-  // });
+    converter.convert((err, result) => {
+      expect(err).to.be.null;
+    });
+  });
+
+  it('Should convert from openapi 3.1 spec to postman collection -- accountService', function() {
+    const fileSource = path.join(__dirname, OPENAPI_31_FOLDER + '/accountService.yaml'),
+      fileData = fs.readFileSync(fileSource, 'utf8'),
+      input = {
+        type: 'string',
+        data: fileData
+      },
+      converter = new SchemaPack(input);
+
+    converter.convert((err, result) => {
+      expect(err).to.be.null;
+    });
+  });
+
+  it('Should convert from openapi 3.1 spec to postman collection -- binLookupService', function() {
+    const fileSource = path.join(__dirname, OPENAPI_31_FOLDER + '/binLookupService.yaml'),
+      fileData = fs.readFileSync(fileSource, 'utf8'),
+      input = {
+        type: 'string',
+        data: fileData
+      },
+      converter = new SchemaPack(input);
+
+    converter.convert((err, result) => {
+      expect(err).to.be.null;
+    });
+  });
 });
