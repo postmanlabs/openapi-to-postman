50ba85ce6894e291355d6b89c5f2d4d61b24f52c
Luis Tejeda <46000487+LuisTejedaS@users.noreply.github.com>
2022-02-09 12:46:31 -0600

    Change validation input when version is 3.1.x
    if the option includeWebhooks is false and there are no paths (only webhooks or componentes) throw error.

--- test/unit/31Xsupport/inputValidation31X.test.js
+++ test/unit/31Xsupport/inputValidation31X.test.js
@@ -69,44 +69,64 @@ const { expect } = require('chai'),
 
 describe('validateSpec method', function () {
   it('should return true with a valid simple spec with webhooks', function () {
-    const validationResult = validateSpec(correctMockedEntryWH);
+    const validationResult = validateSpec(correctMockedEntryWH, { includeWebhooks: true });
     expect(validationResult.result).to.be.true;
   });
 
   it('should return true with a valid simple spec with paths', function () {
-    const validationResult = validateSpec(correctMockedEntryPath);
+    const validationResult = validateSpec(correctMockedEntryPath, { includeWebhooks: true });
     expect(validationResult.result).to.be.true;
   });
 
   it('should return true with a valid simple spec with components', function () {
-    const validationResult = validateSpec(correctMockedEntryComponent);
+    const validationResult = validateSpec(correctMockedEntryComponent, { includeWebhooks: true });
     expect(validationResult.result).to.be.true;
   });
 
   it('should return true with a valid simple spec with components webhooks y paths', function () {
-    const validationResult = validateSpec(correctMockedEntry);
+    const validationResult = validateSpec(correctMockedEntry, { includeWebhooks: true });
     expect(validationResult.result).to.be.true;
   });
 
   it('should return false with an invalid input without openapi field', function () {
-    const validationResult = validateSpec(incorrectMockedEntryNoOpenapi);
+    const validationResult = validateSpec(incorrectMockedEntryNoOpenapi, { includeWebhooks: true });
     expect(validationResult.result).to.be.false;
     expect(validationResult.reason)
       .to.equal('Specification must contain a semantic version number of the OAS specification');
   });
 
   it('should return false with an invalid input without info field', function () {
-    const validationResult = validateSpec(incorrectMockedEntryNoInfo);
+    const validationResult = validateSpec(incorrectMockedEntryNoInfo, { includeWebhooks: true });
     expect(validationResult.result).to.be.false;
     expect(validationResult.reason)
       .to.equal('Specification must contain an Info Object for the meta-data of the API');
   });
 
   it('should return false with an invalid input without path components and webhooks', function () {
-    const validationResult = validateSpec(incorrectMockedEntryNOPathsComponentsWebhooks);
+    const validationResult = validateSpec(incorrectMockedEntryNOPathsComponentsWebhooks, { includeWebhooks: true });
     expect(validationResult.result).to.be.false;
     expect(validationResult.reason)
       .to.equal('Specification must contain either Paths, Webhooks or Components sections');
+  });
+
+  it('should return false with a valid simple spec with only webhooks but include webhooks is false', function () {
+    const validationResult = validateSpec(correctMockedEntryWH, { includeWebhooks: false });
+    expect(validationResult.result).to.be.false;
+  });
+
+  it('should return true with a valid simple spec with paths and webhooks include webhooks is false', function () {
+    const validationResult = validateSpec(correctMockedEntryPath, { includeWebhooks: false });
+    expect(validationResult.result).to.be.true;
+  });
+
+  it('should return false with a valid simple spec with only components and include webhooks is false', function () {
+    const validationResult = validateSpec(correctMockedEntryComponent, { includeWebhooks: false });
+    expect(validationResult.result).to.be.false;
+  });
+
+  it('should return false with input without path components or webhooks include webhooks is false', function () {
+    const validationResult = validateSpec(incorrectMockedEntryNOPathsComponentsWebhooks, { includeWebhooks: false });
+    expect(validationResult.result).to.be.false;
   });
 
 });
--- test/unit/31Xsupport/schemaUtils31X.test.js
+++ test/unit/31Xsupport/schemaUtils31X.test.js
@@ -7,7 +7,7 @@ const { expect } = require('chai'),
 describe('parseSpec method', function () {
   it('should return true and a parsed specification', function () {
     let fileContent = fs.readFileSync(valid31xFolder + '/webhooks.json', 'utf8');
-    const parsedSpec = concreteUtils.parseSpec(fileContent);
+    const parsedSpec = concreteUtils.parseSpec(fileContent, { includeWebhooks: true });
     expect(parsedSpec.result).to.be.true;
     expect(parsedSpec.openapi.openapi).to.equal('3.1.0');
     expect(parsedSpec.openapi.webhooks).to.not.be.undefined;
@@ -15,14 +15,14 @@ describe('parseSpec method', function () {
 
   it('should return false and invalid format message when input content is sent', function () {
     let fileContent = fs.readFileSync(invalid31xFolder + '/empty-spec.yaml', 'utf8');
-    const parsedSpec = concreteUtils.parseSpec(fileContent);
+    const parsedSpec = concreteUtils.parseSpec(fileContent, { includeWebhooks: false });
     expect(parsedSpec.result).to.be.false;
     expect(parsedSpec.reason).to.equal('Invalid format. Input must be in YAML or JSON format.');
   });
 
   it('should return false and Spec must contain info object', function () {
     let fileContent = fs.readFileSync(invalid31xFolder + '/invalid-no-info.json', 'utf8');
-    const parsedSpec = concreteUtils.parseSpec(fileContent);
+    const parsedSpec = concreteUtils.parseSpec(fileContent, { includeWebhooks: false });
     expect(parsedSpec.result).to.be.false;
     expect(parsedSpec.reason).to.equal('Specification must contain an Info Object for the meta-data of the API');
   });
