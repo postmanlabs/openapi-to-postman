4b3555a97f7c21ae63444bc93de857fa557e6f37
Erik Mendoza <efernandomendoza@gmail.com>
2022-02-18 21:44:00 -0600

    Adding invalid value scenario in path variables validation when there are path variables in global servers

--- test/unit/x30schemapack.test.js
+++ test/unit/x30schemapack.test.js
@@ -42,10 +42,36 @@ describe('validateTransaction method', function() {
   });
 
   it('Should validate correctly when a path param in spec does not matches with collection ' +
-    'and there are path variables in servers object (issue#478)', function(done) {
+    'and there are path variables in local servers object (issue#478)', function(done) {
     let issueFolder = path.join(__dirname, VALIDATION_DATA_FOLDER_PATH + '/issues/issue#478'),
       issueSpec = fs.readFileSync(issueFolder + '/local-servers-path-variables-spec.yaml', 'utf-8'),
       issueCollection = fs.readFileSync(issueFolder + '/local-servers-path-variables-collection.json', 'utf-8'),
+      resultObj,
+      historyRequest = [],
+      schemaPack = new Converter.SchemaPack({ type: 'string', data: issueSpec }, { allowUrlPathVarMatching: false });
+
+    getAllTransactions(JSON.parse(issueCollection), historyRequest);
+
+    schemaPack.validateTransaction(historyRequest, (err, result) => {
+      // Schema is sample petsore with one of parameter as empty, expect no mismatch / error
+      expect(err).to.be.null;
+      expect(result).to.be.an('object');
+      resultObj = result.requests[historyRequest[0].id].endpoints[0];
+      expect(resultObj.mismatches).to.have.length(1);
+      expect(resultObj.mismatches[0].reason).to.equal(
+        'Some provided path variables in transaction (petId) does not' +
+        ' match with path variables expected in schema (peterId)'
+      );
+      expect(resultObj.mismatches[0].reasonCode).to.equal('INVALID_VALUE');
+      done();
+    });
+  });
+
+  it('Should validate correctly when a path param in spec does not matches with collection ' +
+    'and there are path variables in global servers object (issue#478)', function(done) {
+    let issueFolder = path.join(__dirname, VALIDATION_DATA_FOLDER_PATH + '/issues/issue#478'),
+      issueSpec = fs.readFileSync(issueFolder + '/global-servers-path-variables-spec.yaml', 'utf-8'),
+      issueCollection = fs.readFileSync(issueFolder + '/global-servers-path-variables-collection.json', 'utf-8'),
       resultObj,
       historyRequest = [],
       schemaPack = new Converter.SchemaPack({ type: 'string', data: issueSpec }, { allowUrlPathVarMatching: false });
