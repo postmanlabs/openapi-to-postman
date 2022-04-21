2312dccd741f8c355b9b8eccad584d9120391792
Luis Tejeda <46000487+LuisTejedaS@users.noreply.github.com>
2022-04-21 15:35:12 -0500

    Add test working from index
    add pipeline from schemapack to related files

--- test/unit/detectRelatedFiles.test.js
+++ test/unit/detectRelatedFiles.test.js
@@ -5,11 +5,13 @@ var expect = require('chai').expect,
   VALID_OPENAPI_PATH = '../data/valid_openapi',
   // VALID_OPENAPI_31_PATH = '../data/valid_openapi31X',
   PET_STORE_SEPARATED = '../data/petstore separate yaml/spec',
+  RELATED_FILES = '../data/relatedFiles',
   // PET_STORE_SEPARATED_JSON = '../data/petstore-separate/spec',
   validPetstore = path.join(__dirname, VALID_OPENAPI_PATH + '/petstore.yaml'),
   // noauth = path.join(__dirname, VALID_OPENAPI_PATH + '/noauth.yaml'),
   // petstoreSeparated = path.join(__dirname, PET_STORE_SEPARATED + '/swagger.yaml'),
-  petstoreSeparatedPet = path.join(__dirname, PET_STORE_SEPARATED + '/Pet.yaml');
+  petstoreSeparatedPet = path.join(__dirname, PET_STORE_SEPARATED + '/Pet.yaml'),
+  missedRef = path.join(__dirname, RELATED_FILES + '/missedRef.yaml');
   // petstoreSeparatedJson = path.join(__dirname, PET_STORE_SEPARATED_JSON + '/swagger.json'),
   // petstoreSeparatedPetJson = path.join(__dirname, PET_STORE_SEPARATED_JSON + '/Pet.json'),
   // validHopService31x = path.join(__dirname, VALID_OPENAPI_31_PATH + '/yaml/hopService.yaml');
@@ -55,5 +57,30 @@ describe('detectRoot method', function() {
     expect(res).to.not.be.empty;
     expect(res.result).to.be.true;
     expect(res.output.data[0].rootFile.path).to.equal('/petstore.yaml');
+  });
+
+  it('should return adjacent and missing nodes', async function () {
+    const contentFileMissedRef = fs.readFileSync(missedRef, 'utf8'),
+      contentFilePet = fs.readFileSync(petstoreSeparatedPet, 'utf8'),
+      input = {
+        type: 'folder',
+        specificationVersion: '3.0',
+        rootFiles: [
+          {
+            path: '/missedRef.yaml',
+            content: contentFileMissedRef
+          }
+        ],
+        data: [
+          {
+            path: 'Pet.yaml',
+            content: contentFilePet
+          }
+        ]
+      },
+      res = await Converter.detectRelatedFiles(input);
+    expect(res).to.not.be.empty;
+    expect(res.result).to.be.true;
+
   });
 });
