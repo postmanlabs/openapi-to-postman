d4c2b9d331ea6659ae2b9ca6838dc07ccff1dc55
Luis Tejeda <46000487+LuisTejedaS@users.noreply.github.com>
2022-04-25 15:41:11 -0500

    calculate missing path and $ref
    calculate missing path and $ref

--- test/unit/relatedFiles.test.js
+++ test/unit/relatedFiles.test.js
@@ -1,5 +1,5 @@
 const { getRelatedFiles, getReferences, getAdjacentAndMissing,
-    calculatePath } = require('./../../lib/relatedFiles'),
+    calculatePath, calculatePathMissing } = require('./../../lib/relatedFiles'),
   expect = require('chai').expect,
   fs = require('fs'),
   path = require('path'),
@@ -10,6 +10,7 @@ const { getRelatedFiles, getReferences, getAdjacentAndMissing,
   swaggerRoot = path.join(__dirname, PET_STORE_SEPARATED + '/swagger.yaml'),
   petstoreSeparatedPet = path.join(__dirname, PET_STORE_SEPARATED + '/Pet.yaml'),
   missedRef = path.join(__dirname, RELATED_FILES + '/missedRef.yaml'),
+  missedRefOut = path.join(__dirname, RELATED_FILES + '/missingOutbounds/missedRefOut.yaml'),
   internalRefOnly = path.join(__dirname, VALID_OPENAPI_PATH + '/deepObjectLengthProperty.yaml');
 
 
@@ -46,7 +47,7 @@ describe('getAdjacentAndMissing function', function () {
     expect(graphAdj.length).to.equal(1);
     expect(graphAdj[0].fileName).to.equal('/Pet.yaml');
     expect(missingNodes.length).to.equal(1);
-    expect(missingNodes[0].path).to.equal('../common/Error.yaml');
+    expect(missingNodes[0].path).to.equal('/common/Error.yaml');
 
   });
 });
@@ -100,8 +101,7 @@ describe('getRelatedFiles function ', function () {
     expect(relatedFiles.length).to.equal(1);
     expect(relatedFiles[0].path).to.equal('/Pet.yaml');
     expect(missingRelatedFiles.length).to.equal(1);
-    expect(missingRelatedFiles[0].path).to.equal('../common/Error.yaml');
-
+    expect(missingRelatedFiles[0].path).to.equal('/common/Error.yaml');
   });
 
   it('should not return internal refs as missing ref', function () {
@@ -116,15 +116,71 @@ describe('getRelatedFiles function ', function () {
     expect(missingRelatedFiles.length).to.equal(0);
   });
 
+  it('should return missing nodes with $ref property', function () {
+    const contentFileMissedRef = fs.readFileSync(missedRefOut, 'utf8'),
+      rootNode = {
+        fileName: '/missedRef.yaml',
+        content: contentFileMissedRef
+      },
+      inputData = [],
+      { relatedFiles, missingRelatedFiles } = getRelatedFiles(rootNode, inputData);
+    expect(relatedFiles.length).to.equal(0);
+    expect(missingRelatedFiles.length).to.equal(2);
+    expect(missingRelatedFiles[0].path).to.equal('/Pet.yaml');
+    expect(missingRelatedFiles[0].$ref).to.be.undefined;
+    expect(missingRelatedFiles[1].path).to.be.undefined;
+    expect(missingRelatedFiles[1].$ref).to.equal('../../common/Error.yaml');
+  });
+
 });
 
 describe('calculatePath function', function () {
-  it('should return the path from the parent', function () {
+  it('should return the path relative to the parent', function () {
     const result = calculatePath('sf/newpet.yaml', '../error.yaml');
     expect(result).to.equal('error.yaml');
   });
-  it('should return localhost:3000 for entry "http://localhost:3000/projects"', function () {
+  it('should return the path relative to the parent inside a folder', function () {
     const result = calculatePath('sf/spec/newpet.yaml', '../common/error.yaml');
     expect(result).to.equal('sf/common/error.yaml');
   });
+});
+
+describe('calculatePathMissing function', function () {
+
+  it('should calculate path and $ref for absolut and relative paths', function () {
+    const result = calculatePathMissing('newpet.yaml', '../../../common/error.yaml'),
+      result1 = calculatePathMissing('sf/newpet.yaml', '../../../common/error.yaml'),
+      result2 = calculatePathMissing('/sf/newpet.yaml', '../../../common/error.yaml'),
+      result3 = calculatePathMissing('/newpet.yaml', '../../../../common/error.yaml'),
+      result4 = calculatePathMissing('newpet.yaml', '/common/error.yaml'),
+      result5 = calculatePathMissing('/missedRef.yaml', 'Pet.yaml'),
+      result6 = calculatePathMissing('/newpet.yaml', '/common/error.yaml'),
+      result7 = calculatePathMissing('/a/sf/newpet.yaml', '../common/error.yaml');
+
+    expect(result.path).to.be.undefined;
+    expect(result.$ref).to.equal('../../../common/error.yaml');
+
+    expect(result1.path).to.be.undefined;
+    expect(result1.$ref).to.equal('../../../common/error.yaml');
+
+    expect(result2.path).to.be.undefined;
+    expect(result2.$ref).to.equal('../../../common/error.yaml');
+
+    expect(result3.path).to.be.undefined;
+    expect(result3.$ref).to.equal('../../../../common/error.yaml');
+
+    expect(result4.$ref).to.be.undefined;
+    expect(result4.path).to.equal('common/error.yaml');
+
+    expect(result5.$ref).to.be.undefined;
+    expect(result5.path).to.equal('/Pet.yaml');
+
+    expect(result6.$ref).to.be.undefined;
+    expect(result6.path).to.equal('/common/error.yaml');
+
+    expect(result7.$ref).to.be.undefined;
+    expect(result7.path).to.equal('/a/common/error.yaml');
+
+  });
+
 });
