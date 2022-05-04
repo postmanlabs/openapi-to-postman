b73fcdfecbff137a017e6ad192aa8f48e70a6ce6
Luis Tejeda <46000487+LuisTejedaS@users.noreply.github.com>
2022-05-04 12:34:01 -0500

    Get related entities
    from an OAS entity get the local $ref entities

---
+++ test/unit/relatedEntities.test.js
@@ -0,0 +1,200 @@
+const { getReferences,
+    isLocalRef,
+    getAdjacentAndMissing,
+    findNodeFromPath,
+    getRelatedEntities } = require('./../../lib/relatedEntity'),
+  expect = require('chai').expect,
+  mockedInputPetstore = {
+    'components': {
+      'schemas': {
+        'Pet': {
+          'required': [
+            'id',
+            'name'
+          ],
+          'properties': {
+            'id': {
+              'type': 'integer',
+              'format': 'int64'
+            },
+            'name': {
+              'type': 'string'
+            },
+            'tag': {
+              'type': 'string'
+            }
+          }
+        },
+        'Pets': {
+          'type': 'array',
+          'items': {
+            '$ref': '#/components/schemas/Pet'
+          }
+        },
+        'Error': {
+          'required': [
+            'code',
+            'message'
+          ],
+          'properties': {
+            'code': {
+              'type': 'integer',
+              'format': 'int32'
+            },
+            'message': {
+              'type': 'string'
+            }
+          }
+        }
+      }
+    }
+  };
+
+
+describe('getReferences function', function () {
+  it('should return 1 local reference from input', function () {
+    const inputNode = {
+        type: 'array',
+        items: {
+          $ref: '#/components/schemas/Pet'
+        }
+      },
+      result = getReferences(inputNode, isLocalRef);
+    expect(result.length).to.equal(1);
+    expect(result[0].path).to.equal('#/components/schemas/Pet');
+
+  });
+
+  it('should return 1 local reference from input, even when repeated', function () {
+    const inputNode = {
+        type: 'object',
+        properties: {
+          pet: { $ref: '#/components/schemas/Pet' },
+          newPet: { $ref: '#/components/schemas/Pet' }
+        }
+      },
+      result = getReferences(inputNode, isLocalRef);
+    expect(result.length).to.equal(1);
+    expect(result[0].path).to.equal('#/components/schemas/Pet');
+  });
+
+  it('should not identify an external ref as local ref', function () {
+    const inputNode = {
+        type: 'object',
+        properties: {
+          pet: { $ref: 'Pet.yaml' },
+          newPet: { $ref: 'Pet.yaml' }
+        }
+      },
+      result = getReferences(inputNode, isLocalRef);
+    expect(result.length).to.equal(0);
+  });
+});
+
+describe('findNodeFromPath method', function () {
+  it('should return localhost:3000 for entry "http://localhost:3000/projects"', function () {
+    const spec = {
+        'components': {
+          'schemas': {
+            'Pet': {
+              'required': [
+                'id',
+                'name'
+              ],
+              'properties': {
+                'id': {
+                  'type': 'integer',
+                  'format': 'int64'
+                },
+                'name': {
+                  'type': 'string'
+                },
+                'tag': {
+                  'type': 'string'
+                }
+              }
+            },
+            'Pets': {
+              'type': 'array',
+              'items': {
+                '$ref': '#/components/schemas/Pet'
+              }
+            },
+            'Error': {
+              'required': [
+                'code',
+                'message'
+              ],
+              'properties': {
+                'code': {
+                  'type': 'integer',
+                  'format': 'int32'
+                },
+                'message': {
+                  'type': 'string'
+                }
+              }
+            }
+          }
+        }
+      },
+      res = findNodeFromPath('#/components/schemas/Pet', spec);
+    expect(res).to.not.be.undefined;
+  });
+});
+
+describe('getAdjacentAndMissing function', function () {
+  it('should return 1 adjacent reference from input', function () {
+    const inputNode = {
+        type: 'array',
+        items: {
+          $ref: '#/components/schemas/Pet'
+        }
+      },
+      { graphAdj, missingNodes } = getAdjacentAndMissing(inputNode, mockedInputPetstore);
+    expect(graphAdj.length).to.equal(1);
+    expect(missingNodes.length).to.equal(0);
+
+  });
+
+  it('should return 1 missing reference from input', function () {
+    const inputNode = {
+        type: 'array',
+        items: {
+          $ref: '#/components/schemas/Dog'
+        }
+      },
+      { graphAdj, missingNodes } = getAdjacentAndMissing(inputNode, mockedInputPetstore);
+    expect(graphAdj.length).to.equal(0);
+    expect(missingNodes.length).to.equal(1);
+    expect(missingNodes[0].$ref).to.equal('#/components/schemas/Dog');
+  });
+});
+
+describe('getRelatedEntities function', function () {
+  it('should return 1 adjacent and the root', function () {
+    const inputNode = {
+        type: 'array',
+        items: {
+          $ref: '#/components/schemas/Pet'
+        }
+      },
+      { relatedEntities, missingRelatedEntities } = getRelatedEntities(inputNode, mockedInputPetstore);
+    expect(relatedEntities.length).to.equal(2);
+    expect(missingRelatedEntities.length).to.equal(0);
+
+  });
+
+  it('should return 1 missing reference from input', function () {
+    const inputNode = {
+        type: 'array',
+        items: {
+          $ref: '#/components/schemas/Dog'
+        }
+      },
+      { relatedEntities, missingRelatedEntities } = getRelatedEntities(inputNode, mockedInputPetstore);
+    expect(relatedEntities.length).to.equal(1);
+    expect(missingRelatedEntities.length).to.equal(1);
+    expect(missingRelatedEntities[0].$ref).to.equal('#/components/schemas/Dog');
+  });
+});
