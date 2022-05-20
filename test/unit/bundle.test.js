4aa298b3992bbca19f25ea9a9dc96dc0374a8272
Erik Mendoza <efernandomendoza@gmail.com>
2022-05-20 16:50:07 -0500

    Adding bundle files proces

--- test/unit/bundle.test.js
+++ test/unit/bundle.test.js
@@ -3,18 +3,48 @@ let expect = require('chai').expect,
   fs = require('fs'),
   path = require('path'),
   BUNDLES_FOLDER = '../data/toBundleExamples',
-  swaggerToBundleFolder = path.join(__dirname, BUNDLES_FOLDER + '/swagger-multi-file');
+  swaggerMultifileFolder = path.join(__dirname, BUNDLES_FOLDER + '/swagger-multi-file'),
+  localRefFolder = path.join(__dirname, BUNDLES_FOLDER + '/local_ref'),
+  easyFolder = path.join(__dirname, BUNDLES_FOLDER + '/swagger-multi-file_easy');
 
-describe('detectRelatedFiles method', function () {
+describe('bundle files method', function () {
+  it('Should return bundled file with an schema called from a response', async function () {
+    let contentRootFile = fs.readFileSync(easyFolder + '/root.yaml', 'utf8'),
+      user = fs.readFileSync(easyFolder + '/schemas/user.yaml', 'utf8'),
+      input = {
+        type: 'folder',
+        specificationVersion: '3.0',
+        rootFiles: [
+          {
+            path: '/root.yaml',
+            content: contentRootFile
+          }
+        ],
+        data: [
+          {
+            path: '/schemas/user.yaml',
+            content: user
+          }
+        ]
+      };
+    const res = await Converter.bundle(input);
+    expect(res).to.not.be.empty;
+    expect(res.result).to.be.true;
+    expect(res.output.data.bundledContent.paths['/users/{userId}'].get.responses['200']
+      .content['application/json'].schema.$ref)
+      .to.be.equal('#/components/schema/~1schemas~1user.yaml');
+    expect(Object.keys(res.output.data.bundledContent.components.schemas['/schemas/user.yaml']))
+      .to.have.members(['type', 'properties']);
+  });
 
-  it('Should return bundled file', async function () {
-    let contentRootFile = fs.readFileSync(swaggerToBundleFolder + '/v1.yaml', 'utf8'),
-      responses = fs.readFileSync(swaggerToBundleFolder + '/responses.yaml', 'utf8'),
-      schemasIndex = fs.readFileSync(swaggerToBundleFolder + '/schemas/index.yaml', 'utf8'),
-      schemasUser = fs.readFileSync(swaggerToBundleFolder + '/schemas/user.yaml', 'utf8'),
-      schemasClient = fs.readFileSync(swaggerToBundleFolder + '/schemas/client.yaml', 'utf8'),
-      toySchema = fs.readFileSync(swaggerToBundleFolder + '/otherSchemas/toy.yaml', 'utf8'),
-      userProps = fs.readFileSync(swaggerToBundleFolder + '/userProps.yaml', 'utf8'),
+  it('Should return bundled file from root with components with', async function () {
+    let contentRootFile = fs.readFileSync(swaggerMultifileFolder + '/v1.yaml', 'utf8'),
+      responses = fs.readFileSync(swaggerMultifileFolder + '/responses.yaml', 'utf8'),
+      schemasIndex = fs.readFileSync(swaggerMultifileFolder + '/schemas/index.yaml', 'utf8'),
+      schemasUser = fs.readFileSync(swaggerMultifileFolder + '/schemas/user.yaml', 'utf8'),
+      schemasClient = fs.readFileSync(swaggerMultifileFolder + '/schemas/client.yaml', 'utf8'),
+      toySchema = fs.readFileSync(swaggerMultifileFolder + '/otherSchemas/toy.yaml', 'utf8'),
+      userProps = fs.readFileSync(swaggerMultifileFolder + '/userProps.yaml', 'utf8'),
       input = {
         type: 'folder',
         specificationVersion: '3.0',
@@ -48,6 +78,45 @@ describe('detectRelatedFiles method', function () {
           {
             path: '/userProps.yaml',
             content: userProps
+          }
+        ]
+      };
+    const res = await Converter.bundle(input);
+    expect(res).to.not.be.empty;
+    expect(res.result).to.be.true;
+  });
+
+  it('Should return bundled file from a file with local references', async function () {
+    let contentRootFile = fs.readFileSync(localRefFolder + '/root.yaml', 'utf8'),
+      responses = fs.readFileSync(localRefFolder + '/responses.yaml', 'utf8'),
+      schemasIndex = fs.readFileSync(localRefFolder + '/schemas/index.yaml', 'utf8'),
+      schemasClient = fs.readFileSync(localRefFolder + '/schemas/client.yaml', 'utf8'),
+      toySchema = fs.readFileSync(localRefFolder + '/otherSchemas/toy.yaml', 'utf8'),
+      input = {
+        type: 'folder',
+        specificationVersion: '3.0',
+        rootFiles: [
+          {
+            path: '/root.yaml',
+            content: contentRootFile
+          }
+        ],
+        data: [
+          {
+            path: '/responses.yaml',
+            content: responses
+          },
+          {
+            path: '/schemas/index.yaml',
+            content: schemasIndex
+          },
+          {
+            path: '/schemas/client.yaml',
+            content: schemasClient
+          },
+          {
+            path: '/otherSchemas/toy.yaml',
+            content: toySchema
           }
         ]
       };
--- test/unit/jsonPointer.test.js
+++ test/unit/jsonPointer.test.js
@@ -14,17 +14,14 @@ describe('getKeyInComponents function', function () {
     expect(result[2]).to.equal('pet.yaml');
   });
 
-  // TODOE: support this test
-  // it('should return ["components", "schemas", "pet.yaml", "definitions", "world"] when is pointing to a local ref',
-  //   function () {
-  //     const result = getKeyInComponents(['components', 'schemas'], 'pet.yaml', '/definitions/world');
-  //     expect(result.length).to.equal(5);
-  //     expect(result[0]).to.equal('components');
-  //     expect(result[1]).to.equal('schemas');
-  //     expect(result[2]).to.equal('pet.yaml');
-  //     expect(result[3]).to.equal('definitions');
-  //     expect(result[4]).to.equal('world');
-  //   });
+  it('should return ["components", "schemas", "pet.yaml", "definitions", "world"] when is pointing to a local ref',
+    function () {
+      const result = getKeyInComponents(['components', 'schemas'], 'pet.yaml', '/definitions/world');
+      expect(result.length).to.equal(3);
+      expect(result[0]).to.equal('components');
+      expect(result[1]).to.equal('schemas');
+      expect(result[2]).to.equal('pet.yaml#/definitions/world');
+    });
 
   it('should return ["components", "schemas", "folder/pet.yaml"] when there is an scaped slash', function () {
     const result = getKeyInComponents(['components', 'schemas'], 'folder~1pet.yaml');
@@ -41,26 +38,23 @@ describe('getJsonPointerRelationToRoot function', function () {
     let res = getJsonPointerRelationToRoot(
       jsonPointerEncodeAndReplace,
       'Pets.yaml',
-      'Pets.yaml',
-      ['components', 'schemas', 'Pets.yaml']
+      ['schemas', 'Pets.yaml']
     );
     expect(res).to.equal('#/components/schemas/Pets.yaml');
   });
-  it('should return "#/components/schemas/hello.yaml/definitions/world" no local path and schema', function () {
+  it('should return "#/components/schemas/hello.yaml" no local path and schema', function () {
     let res = getJsonPointerRelationToRoot(
       jsonPointerEncodeAndReplace,
-      'hello.yaml',
       'hello.yaml#/definitions/world',
-      ['components', 'schemas', 'hello.yaml']
+      ['schemas', 'hello.yaml']
     );
-    expect(res).to.equal('#/components/schemas/hello.yaml/definitions/world');
+    expect(res).to.equal('#/components/schemas/hello.yaml');
   });
   it('should return "#/components/schemas/Error" no file path', function () {
     let res = getJsonPointerRelationToRoot(
       jsonPointerEncodeAndReplace,
-      '',
       '#/components/schemas/Error',
-      ['components', 'schemas']
+      ['components', 'schemas', 'Error']
     );
     expect(res).to.equal('#/components/schemas/Error');
   });
@@ -70,8 +64,7 @@ describe('concatJsonPointer function ', function () {
   it('should return "#/components/schemas/Pets.yaml" no local path and schema', function () {
     let res = concatJsonPointer(
       jsonPointerEncodeAndReplace,
-      'Pets.yaml',
-      ['components', 'schemas', 'Pets.yaml']
+      ['schemas', 'Pets.yaml']
     );
     expect(res).to.equal('#/components/schemas/Pets.yaml');
   });
@@ -79,34 +72,29 @@ describe('concatJsonPointer function ', function () {
   it('should return "#/components/schemas/other~1Pets.yaml" no local path and schema folder in filename', function () {
     let res = concatJsonPointer(
       jsonPointerEncodeAndReplace,
-      'other/Pets.yaml',
-      ['components', 'schemas', 'other/Pets.yaml']
+      ['schemas', 'other/Pets.yaml']
     );
     expect(res).to.equal('#/components/schemas/other~1Pets.yaml');
   });
   it('should return "#/components/schemas/some~1Pet" no local path and schema folder in filename', function () {
     let res = concatJsonPointer(
       jsonPointerEncodeAndReplace,
-      'some/Pet.yaml',
-      ['components', 'schemas', 'some/Pet.yaml']
+      ['schemas', 'some/Pet.yaml']
     );
     expect(res).to.equal('#/components/schemas/some~1Pet.yaml');
   });
-  it('should return "#/components/schemas/hello.yaml/definitions/world" no local path and schema', function () {
+  it('should return "#/components/schemas/hello.yaml" no local path and schema', function () {
     let res = concatJsonPointer(
       jsonPointerEncodeAndReplace,
-      'hello.yaml',
-      ['components', 'schemas', 'hello.yaml'],
-      '/definitions/world'
+      ['schemas', 'hello.yaml']
     );
-    expect(res).to.equal('#/components/schemas/hello.yaml/definitions/world');
+    expect(res).to.equal('#/components/schemas/hello.yaml');
   });
 
   it('should return "#/components/schemas/~1Pets.yaml" no local path and schema', function () {
     let res = concatJsonPointer(
       jsonPointerEncodeAndReplace,
-      '/Pets.yaml',
-      ['components', 'schemas', '/Pets.yaml']
+      ['schemas', '/Pets.yaml']
     );
     expect(res).to.equal('#/components/schemas/~1Pets.yaml');
   });
