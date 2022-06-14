6ade03062ad569d63823b93963884f5da212f9a3
Luis Tejeda <46000487+LuisTejedaS@users.noreply.github.com>
2022-06-14 12:25:48 -0500

    Add reusable referenced path item object
    Add reusable referenced path item object

---
+++ test/unit/bundle31.test.js
@@ -0,0 +1,42 @@
+const expect = require('chai').expect,
+  Converter = require('../../index.js'),
+  fs = require('fs'),
+  path = require('path'),
+  BUNDLES_FOLDER = '../data/toBundleExamples',
+  pathItem31 = path.join(__dirname, BUNDLES_FOLDER + '/referenced_paths_31');
+
+
+describe('bundle files method - 3.1', function () {
+  it('Should return bundled file as json - Path item object', async function () {
+    let contentRootFile = fs.readFileSync(pathItem31 + '/root.yaml', 'utf8'),
+      user = fs.readFileSync(pathItem31 + '/path.yaml', 'utf8'),
+      expected = fs.readFileSync(pathItem31 + '/expected.json', 'utf8'),
+      input = {
+        type: 'multiFile',
+        specificationVersion: '3.1',
+        rootFiles: [
+          {
+            path: '/root.yaml'
+          }
+        ],
+        data: [
+          {
+            path: '/root.yaml',
+            content: contentRootFile
+          },
+          {
+            path: '/path.yaml',
+            content: user
+          }
+        ],
+        options: {},
+        bundleFormat: 'JSON'
+      };
+    const res = await Converter.bundle(input);
+
+    expect(res).to.not.be.empty;
+    expect(res.result).to.be.true;
+    expect(res.output.specification.version).to.equal('3.1');
+    expect(JSON.stringify(res.output.data[0].bundledContent, null, 2)).to.be.equal(expected);
+  });
+});
