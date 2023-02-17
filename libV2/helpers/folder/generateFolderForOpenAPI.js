37dead783898171e0df09fa5b7c058d865291465
Avishek Saha <avishek.saha@postman.com>
2023-02-17 12:33:17 +0530

    Added Default item array in folder

--- libV2/helpers/folder/generateFolderForOpenAPI.js
+++ libV2/helpers/folder/generateFolderForOpenAPI.js
@@ -3,7 +3,8 @@ const _ = require('lodash');
 module.exports = function (openapi, node) {
   return {
     data: {
-      name: _.get(node, 'meta.path', 'FOLDER')
+      name: _.get(node, 'meta.path', 'FOLDER'),
+      item: []
     }
   };
 };
