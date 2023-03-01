cb5f64f950bd08acc423295aa609c75508b9cd71
Vishal Shingala <vishalb.shingala@gmail.com>
2023-03-01 17:58:28 +0530

    Fixed issue where for certain schemas, having both pattern and minLength was generating incorrect data

--- assets/json-schema-faker.js
+++ assets/json-schema-faker.js
@@ -23830,7 +23830,7 @@ function extend() {
               var min = Math.max(params.minLength || 0, 0);
               var max = Math.min(params.maxLength || Infinity, Infinity);
               while (value.length < min) {
-                  value += ' ' + value;
+                  value += (schema.pattern ? '' : ' ') + value;
               }
               if (value.length > max) {
                   value = value.substr(0, max);
@@ -24236,12 +24236,12 @@ function extend() {
   // Updated objectType definition to latest version (0.5.0-rcv.41)
   var objectType = function objectType(value, path, resolve, traverseCallback, seenSchemaCache) {
     const props = {};
-  
+
     const properties = value.properties || {};
     const patternProperties = value.patternProperties || {};
     const requiredProperties = typeof value.required === 'boolean' ? [] : (value.required || []).slice();
     const allowsAdditional = value.additionalProperties !== false;
-  
+
     const propertyKeys = Object.keys(properties);
     const patternPropertyKeys = Object.keys(patternProperties);
     const optionalProperties = propertyKeys.concat(patternPropertyKeys).reduce((_response, _key) => {
@@ -24249,11 +24249,11 @@ function extend() {
       return _response;
     }, []);
     const allProperties = requiredProperties.concat(optionalProperties);
-  
+
     const additionalProperties = allowsAdditional // eslint-disable-line
       ? (value.additionalProperties === true ? anyType : value.additionalProperties)
       : value.additionalProperties;
-  
+
     if (!allowsAdditional
       && propertyKeys.length === 0
       && patternPropertyKeys.length === 0
@@ -24262,28 +24262,28 @@ function extend() {
       // just nothing
       return null;
     }
-  
+
     if (optionAPI('requiredOnly') === true) {
       requiredProperties.forEach(key => {
         if (properties[key]) {
           props[key] = properties[key];
         }
       });
-  
+
       return traverseCallback(props, path.concat(['properties']), resolve, value, seenSchemaCache);
     }
-  
+
     const optionalsProbability = optionAPI('alwaysFakeOptionals') === true ? 1.0 : optionAPI('optionalsProbability');
     const fixedProbabilities = optionAPI('alwaysFakeOptionals') || optionAPI('fixedProbabilities') || false;
     const ignoreProperties = optionAPI('ignoreProperties') || [];
     const reuseProps = optionAPI('reuseProperties');
     const fillProps = optionAPI('fillProperties');
-  
+
     const max = value.maxProperties || (allProperties.length + (allowsAdditional ? random.number(1, 5) : 0));
-  
+
     let min = Math.max(value.minProperties || 0, requiredProperties.length);
     let neededExtras = Math.max(0, allProperties.length - min);
-  
+
     if (optionalsProbability !== null) {
       if (fixedProbabilities === true) {
         neededExtras = Math.round((min - requiredProperties.length) + (optionalsProbability * (allProperties.length - min)));
@@ -24291,21 +24291,21 @@ function extend() {
         neededExtras = random.number(min - requiredProperties.length, optionalsProbability * (allProperties.length - min));
       }
     }
-  
+
     const extraPropertiesRandomOrder = random.shuffle(optionalProperties).slice(0, neededExtras);
     const extraProperties = optionalProperties.filter(_item => {
       return extraPropertiesRandomOrder.indexOf(_item) !== -1;
     });
-  
+
     // properties are read from right-to-left
     const _limit = optionalsProbability !== null || requiredProperties.length === max ? max : random.number(0, max);
     const _props = requiredProperties.concat(extraProperties).slice(0, max);
     const _defns = [];
-  
+
     if (value.dependencies) {
       Object.keys(value.dependencies).forEach(prop => {
         const _required = value.dependencies[prop];
-  
+
         if (_props.indexOf(prop) !== -1) {
           if (Array.isArray(_required)) {
             // property-dependencies
@@ -24319,20 +24319,20 @@ function extend() {
           }
         }
       });
-  
+
       // schema-dependencies
       if (_defns.length) {
         delete value.dependencies;
-  
+
         return traverseCallback({
           allOf: _defns.concat(value),
         }, path.concat(['properties']), resolve, value, seenSchemaCache);
       }
     }
-  
+
     const skipped = [];
     const missing = [];
-  
+
     _props.forEach(key => {
       for (let i = 0; i < ignoreProperties.length; i += 1) {
         if ((ignoreProperties[i] instanceof RegExp && ignoreProperties[i].test(key))
@@ -24342,24 +24342,24 @@ function extend() {
           return;
         }
       }
-  
+
       if (additionalProperties === false) {
         if (requiredProperties.indexOf(key) !== -1) {
           props[key] = properties[key];
         }
       }
-  
+
       if (properties[key]) {
         props[key] = properties[key];
       }
-  
+
       let found;
-  
+
       // then try patternProperties
       patternPropertyKeys.forEach(_key => {
         if (key.match(new RegExp(_key))) {
           found = true;
-  
+
           if (props[key]) {
             utils.merge(props[key], patternProperties[_key]);
           } else {
@@ -24367,13 +24367,13 @@ function extend() {
           }
         }
       });
-  
+
       if (!found) {
         // try patternProperties again,
         const subschema = patternProperties[key] || additionalProperties;
-  
+
         // FIXME: allow anyType as fallback when no subschema is given?
-  
+
         if (subschema && additionalProperties !== false) {
           // otherwise we can use additionalProperties?
           props[patternProperties[key] ? random.randexp(key) : key] = properties[key] || subschema;
@@ -24382,54 +24382,54 @@ function extend() {
         }
       }
     });
-  
+
     // discard already ignored props if they're not required to be filled...
     let current = Object.keys(props).length + (fillProps ? 0 : skipped.length);
-  
+
     // generate dynamic suffix for additional props...
     const hash = suffix => random.randexp(`_?[_a-f\\d]{1,3}${suffix ? '\\$?' : ''}`);
-  
+
     function get(from) {
       let one;
-  
+
       do {
         if (!from.length) break;
         one = from.shift();
       } while (props[one]);
-  
+
       return one;
     }
-  
+
     let minProps = min;
     if (allowsAdditional && !requiredProperties.length) {
       minProps = Math.max(optionalsProbability === null || additionalProperties ? random.number(fillProps ? 1 : 0, max) : 0, min);
     }
-  
+
     while (fillProps) {
       if (!(patternPropertyKeys.length || allowsAdditional)) {
         break;
       }
-  
+
       if (current >= minProps) {
         break;
       }
-  
+
       if (allowsAdditional) {
         if (reuseProps && ((propertyKeys.length - current) > minProps)) {
           let count = 0;
           let key;
-  
+
           do {
             count += 1;
-  
+
             // skip large objects
             if (count > 1000) {
               break;
             }
-  
+
             key = get(requiredProperties) || random.pick(propertyKeys);
           } while (typeof props[key] !== 'undefined');
-  
+
           if (typeof props[key] === 'undefined') {
             props[key] = properties[key];
             current += 1;
@@ -24437,48 +24437,48 @@ function extend() {
         } else if (patternPropertyKeys.length && !additionalProperties) {
           const prop = random.pick(patternPropertyKeys);
           const word = random.randexp(prop);
-  
+
           if (!props[word]) {
             props[word] = patternProperties[prop];
             current += 1;
           }
         } else {
           const word = get(requiredProperties) || (wordsGenerator(1) + hash());
-  
+
           if (!props[word]) {
             props[word] = additionalProperties || anyType;
             current += 1;
           }
         }
       }
-  
+
       for (let i = 0; current < min && i < patternPropertyKeys.length; i += 1) {
         const _key = patternPropertyKeys[i];
         const word = random.randexp(_key);
-  
-  
+
+
         if (!props[word]) {
           props[word] = patternProperties[_key];
           current += 1;
         }
       }
     }
-  
+
     // fill up-to this value and no more!
     if (requiredProperties.length === 0 && (!allowsAdditional || optionalsProbability === false)) {
       const maximum = random.number(min, max);
-  
+
       for (; current < maximum;) {
         const word = get(propertyKeys);
-  
+
         if (word) {
           props[word] = properties[word];
         }
-  
+
         current += 1;
       }
     }
-  
+
     return traverseCallback(props, path.concat(['properties']), resolve, value, seenSchemaCache);
   };
 
@@ -24571,7 +24571,7 @@ function extend() {
 
       // types from draft-0[67] (?)
       'uri-reference': `${URI_PATTERN}${PARAM_PATTERN}`,
-      'uri-template': URI_PATTERN.replace('(?:', '(?:/\\{[a-z][:a-zA-Z0-9-]*\\}|'),
+      'uri-template': URI_PATTERN.replace('(?:', '(?:/\\{[a-z][a-zA-Z0-9]*\\}|'),
       'json-pointer': `(/(?:${FRAGMENT.replace(']*', '/]*')}|~[01]))+`,
 
       // some types from https://github.com/OAI/OpenAPI-Specification/blob/master/versions/3.0.1.md#data-types (?)
