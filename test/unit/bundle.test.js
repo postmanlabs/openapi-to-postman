37003c12522496da978f19a85ccf92f994c195a0
Prashant Raghu <prashant.raghu@postman.com>
2023-08-17 21:25:46 +0530

    Added handling for circular refs

--- test/unit/bundle.test.js
+++ test/unit/bundle.test.js
@@ -2943,6 +2943,47 @@ describe('bundle files method - 3.0', function () {
       expect(res.output.specification.version).to.equal('3.0');
       expect(JSON.stringify(JSON.parse(res.output.data[0].bundledContent), null, 2)).to.be.equal(expected);
     });
+
+  it('Should return bundled file as json with circular url refs - remote_url_refs', async function () {
+    let contentRootFile = fs.readFileSync(remoteURLRefExamples + '/circular/root.json', 'utf8'),
+      schema = fs.readFileSync(remoteURLRefExamples + '/circular/schema.json', 'utf8'),
+
+      remoteRefResolver = async (refURL) => {
+        if (refURL.includes('schema')) {
+          return JSON.parse(schema);
+        }
+
+        if (refURL.includes('root')) {
+          return JSON.parse(contentRootFile);
+        }
+      },
+      expected = fs.readFileSync(remoteURLRefExamples + '/circular/expected.json', 'utf8'),
+      input = {
+        type: 'multiFile',
+        specificationVersion: '3.0',
+        rootFiles: [
+          {
+            path: 'root.json'
+          }
+        ],
+        data: [
+          {
+            path: 'root.json',
+            content: contentRootFile
+          }
+        ],
+        options: {},
+        bundleFormat: 'JSON',
+        remoteRefResolver
+      };
+
+    const res = await Converter.bundle(input);
+
+    expect(res).to.not.be.empty;
+    expect(res.result).to.be.true;
+    expect(res.output.specification.version).to.equal('3.0');
+    expect(JSON.stringify(JSON.parse(res.output.data[0].bundledContent), null, 2)).to.be.equal(expected);
+  });
 });
 
 describe('getReferences method when node does not have any reference', function() {
