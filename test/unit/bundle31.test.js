e42090c1ae059ce176dd664bcc7be30223024c2f
barshan23 <avishek.saha@postman.com>
2024-01-03 10:56:30 +0530

    Fixed a bug where incorrect ref in Paths was not being handled correctly

--- test/unit/bundle31.test.js
+++ test/unit/bundle31.test.js
@@ -4,6 +4,7 @@ const expect = require('chai').expect,
   path = require('path'),
   BUNDLES_FOLDER = '../data/toBundleExamples',
   pathItem31 = path.join(__dirname, BUNDLES_FOLDER + '/referenced_paths_31'),
+  invalidPathRef31 = path.join(__dirname, BUNDLES_FOLDER + '/referenced_invalid_paths_31'),
   webhookItem31 = path.join(__dirname, BUNDLES_FOLDER + '/referenced_webhook_31');
 
 
@@ -65,6 +66,36 @@ describe('bundle files method - 3.1', function () {
         ],
         options: {},
         bundleFormat: 'JSON'
+      };
+    const res = await Converter.bundle(input);
+
+    expect(res).to.not.be.empty;
+    expect(res.result).to.be.true;
+    expect(res.output.specification.version).to.equal('3.1');
+    expect(JSON.stringify(JSON.parse(res.output.data[0].bundledContent), null, 2)).to.be.equal(expected);
+  });
+
+  it('Should return bundled file where path has an invalid $ref', async function () {
+    let contentRootFile = fs.readFileSync(invalidPathRef31 + '/invalid_reference_at_path.yaml', 'utf8'),
+      expected = fs.readFileSync(invalidPathRef31 + '/expected.json', 'utf8'),
+      input = {
+        origin: 'browser',
+        type: 'multiFile',
+        specificationVersion: '3.1',
+        options: { includeReferenceMap: true },
+        bundleFormat: 'JSON',
+        rootFiles: [
+          {
+            path: 'root.yaml',
+            content: contentRootFile
+          }
+        ],
+        data: [
+          {
+            path: 'root.yaml',
+            content: contentRootFile
+          }
+        ]
       };
     const res = await Converter.bundle(input);
 
